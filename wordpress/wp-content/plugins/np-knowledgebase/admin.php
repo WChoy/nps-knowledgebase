b078e2a4b30cc3eaa1e3dcd25302f5a81b48eeb5
William Choy <wchoy@northps.com>
2013-03-07 16:25:04 -0500

    Site plugin.

---
+++ wordpress/wp-content/plugins/np-knowledgebase/admin.php
@@ -0,0 +1,58 @@
+<?php
+
+
+/**
+ *  Add a category metabox – http://shibashake.com/wordpress-theme/add-tags-and-categories-to-your-wordpress-page
+ **/
+add_action('admin_init', 'np_knowledgebase_admin_init');
+
+function np_knowledgebase_admin_init() {
+  global $wp_version;
+
+  //  metaboxes automatically get added as part of the register_taxonomy_for_object_type function.
+  // Saving Tags and Categories for WordPress Pages
+  register_taxonomy_for_object_type('category', 'page');
+}
+
+/**  Add a category metabox to WordPress page. **/
+
+
+/**
+ *  CHANGE HOWDY – http://www.wpbeginner.com/wp-tutorials/how-to-change-the-howdy-text-in-wordpress-3-3-admin-bar/
+ **/
+add_action( 'admin_bar_menu', 'np_knowledgebase_admin_bar_my_custom_account_menu', 11 );
+
+function np_knowledgebase_admin_bar_my_custom_account_menu( $wp_admin_bar ) {
+  $user_id = get_current_user_id();
+  $current_user = wp_get_current_user();
+  $profile_url = get_edit_profile_url( $user_id );
+
+  if ( 0 != $user_id ) {
+    /* Add the "My Account" menu */
+    $avatar = get_avatar( $user_id, 28 );
+    $howdy = sprintf( __('Welcome %1$s'), $current_user->display_name );
+    $class = empty( $avatar ) ? '' : 'with-avatar';
+
+    $wp_admin_bar->add_menu( array(
+      'id' => 'my-account',
+      'parent' => 'top-secondary',
+      'title' => $howdy . $avatar,
+      'href' => $profile_url,
+      'meta' => array('class' => $class),
+    ));
+  }
+}
+
+/** END CHANGE HOWDY **/
+
+/**
+ * Add Metabox for Resources
+ */
+add_action( 'add_meta_boxes', 'prowp_meta_box_init' );
+// meta box functions for adding the meta box and saving the data
+function prowp_meta_box_init() {
+// create our custom meta box
+add_meta_box( 'prowp-meta', 'Product Information',
+'prowp_meta_box', 'post', 'side', 'default' );
+}
+?>
---
+++ wordpress/wp-content/plugins/np-knowledgebase/np-knowledgebase.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package Akismet
+ */
+/*
+Plugin Name: np-knownledgebase
+Plugin URI: http://kb.northps.com/
+Description: NorthPoint's Internal Operations Knowledgebase site plugin.
+Version: 1.0.0
+Author: William Choy
+Author URI: mailto:William_choy@northps.com?Subject=np-knownledgebase
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+define('NP_KNOWLEDGEBASE_VERSION', '2.5.6');
+define('NP_KNOWLEDGEBASE_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+if ( is_admin() ) {
+	require_once dirname( __FILE__ ) . '/admin.php';
+}
+
+
+//add_filter('gettext', 'change_howdy', 10, 3);
+//
+//function change_howdy($translated, $text, $domain) {
+//  if (!is_admin() || 'default' != $domain)
+//    return $translated;
+//  if (false !== strpos($translated, 'Howdy'))
+//    return str_replace('Howdy', 'Welcome back', $translated);
+//  return $translated;
+//}
+
+/**
+ *  Add a category metabox – http://shibashake.com/wordpress-theme/add-tags-and-categories-to-your-wordpress-page
+ **/
+// Retrieve Both Posts and Pages for Category Links
+add_filter('request', 'np_knowledgebase_expanded_request');
+
+function np_knowledgebase_expanded_request($q) {
+  if (isset($q['tag']) || isset($q['category_name']))
+              $q['post_type'] = array('post', 'page');
+  return $q;
+}
+/**  Add a category metabox to WordPress page. **/
+
+
+/**
+ *  http://en.forums.wordpress.com/topic/how-to-create-submenus-of-a-category-please
+ */
+?>
