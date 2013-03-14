874dbfe8eee2a0948b614c825229d04205df2326
William Choy <wchoy@northps.com>
2013-03-14 16:23:14 -0400

    Fix breadcrumbs and its positioning.

--- wordpress/wp-content/themes/wordpress-bootstrap/functions.php
+++ wordpress/wp-content/themes/wordpress-bootstrap/functions.php
@@ -69,7 +69,7 @@ function bones_register_sidebars() {
     register_sidebar(array(
       'id' => 'header',
       'name' => 'Header',
-      'before_widget' => '<div id="%1$s" class="widget span12 %2$s">',
+      'before_widget' => '<div id="%1$s" class="widget %2$s">',
       'after_widget' => '</div>',
       'before_title' => '<h4 class="widgettitle">',
       'after_title' => '</h4>',
